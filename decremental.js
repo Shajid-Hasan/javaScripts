c00ed68855e8d7d0c72bfb47f4d8b6949f0a8649
Shajid <hasan.shajid222@gmail.com>
2024-09-10 23:36:28 +0600

    added new

--- decremental.js
+++ decremental.js
@@ -1,3 +1,9 @@
-for(let i=20; i>=0; i--){
-    console.log(i);
+
+for(let i=1; i<20; i++){
+    if(i % 2 ===0){
+        console.log('Even number',+ i);
+    }
+    else{
+        console.log('Odd number ', + i);
+    }
 }
