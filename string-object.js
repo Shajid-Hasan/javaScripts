952e6e2a84000caba8050754e96c6fc6bad60a79
Shajid <hasan.shajid222@gmail.com>
2024-09-11 14:30:51 +0600

    added new

---
+++ string-object.js
@@ -0,0 +1,10 @@
+const school = 'Mithapukur high school';
+const subject = 'chemistry';
+const book = 'chemistry';
+console.log(school);
+if(subject !== book){
+    console.log('ami eibar exam fatay falamu');
+}
+else{
+    console.log('hudai pore lav ki');
+}
