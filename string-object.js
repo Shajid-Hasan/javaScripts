611e3b5c26fd44672494201e7ad37c448a8e12b1
Shajid <hasan.shajid222@gmail.com>
2024-09-11 15:34:25 +0600

    added new

--- string-object.js
+++ string-object.js
@@ -1,10 +1,20 @@
-const school = 'Mithapukur high school';
-const subject = 'chemistry';
-const book = 'chemistry';
-console.log(school);
-if(subject !== book){
-    console.log('ami eibar exam fatay falamu');
+// const school = 'Mithapukur high school';
+// const subject = 'chemistry';
+// const book = 'chemistry';
+// console.log(school);
+// if(subject !== book){
+//     console.log('ami eibar exam fatay falamu');
+// }
+// else{
+//     console.log('hudai pore lav ki');
+// }
+
+const drink = 'water'
+const liquid = 'water  ';
+
+if(drink.trim() === liquid.trim()){
+    console.log('panir opor nam jibon');
 }
 else{
-    console.log('hudai pore lav ki');
+    console.log('somudre pani ache but khaite pari na');
 }
