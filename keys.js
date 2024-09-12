e2a596ee7ab67ef218b52bfd049d34709ec89ecc
Shajid <hasan.shajid222@gmail.com>
2024-09-13 01:59:46 +0600

    added new

---
+++ keys.js
@@ -0,0 +1,27 @@
+
+// const computer = {
+//     brand: 'hp',
+//     color: 'metal',
+//     processor: 'intel',
+//     price: '90000',
+//     hdd : '512gb'
+// }
+
+
+// console.log(computer);
+// const keys = object.keys(computer);
+// console.log(keys);
+
+const college = {
+    name: 'lgb',
+    class: ['11, 12'],
+    events: ['science fair', 'nijoydibos', '21 feb'],
+    unique: {
+        color: 'blue',
+        result: 5,
+        merit: 'top'
+    }
+}
+// console.log(college.unique.color)
+college.unique.result.merit = 'top top top most'
+console.log(college.unique);
---
+++ looping.js
@@ -0,0 +1,9 @@
+
+const friends = ['elon', 'bill', 'mark', 'waren'];
+for(const friend of friends){
+    // console.log(friend);
+}
+for(let i=0; i<friends.length; i++){
+    console.log(i);
+    console.log(friends[i]);
+}
