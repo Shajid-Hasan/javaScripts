e901fa2f47d1432a7755c4c06c2290bd0984e86f
Shajid <hasan.shajid222@gmail.com>
2024-09-15 18:04:14 +0600

    added new

---
+++ verious-peramiter.js
@@ -0,0 +1,16 @@
+
+function evenSizedString (str){
+    const size = str.length;
+    if(size % 2 === 0){
+        console.log('even size');
+        return true;
+    }
+    else{
+        console.log('odd size');
+        return false;
+    }
+    console.log(str, size);
+}
+
+evenSizedString('dhaka');
+evenSizedString('rangpur');
