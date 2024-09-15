7f15368df2eb6c9ab1740b737403f6dd045a9372
Shajid <hasan.shajid222@gmail.com>
2024-09-15 17:44:51 +0600

    added new

---
+++ argument.js
@@ -0,0 +1,7 @@
+
+function doubleIt (number){
+    const doubled = number * 2;
+    console.log(number, doubled);
+}
+console.log('i will call the function');
+doubleIt(20);
---
+++ odd-evenf.js
@@ -0,0 +1,17 @@
+function isEven (num){
+    
+    if(num % 2 === 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+// const num2 = isEven(20);
+// console.log(num2);
+// const num3 = isEven(35);
+// console.log(num3);
+
+console.log(isEven(5));
+console.log(isEven(10));
